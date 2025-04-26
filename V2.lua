db80b24cc682436e00a7cec955777534ac3c6f4f
Jayver-Dev <jaydenmiller290@gmail.com>
2025-04-26 15:22:40 -0500

    Update V2.lua

--- V2.lua
+++ V2.lua
@@ -1,4 +1,4 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/CustomFIeld/main/RayField.lua'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
